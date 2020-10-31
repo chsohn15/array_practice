56b1b6bb55c830c7386017586831c9479b1d070f
chsohn15 <chsohn15@gmail.com>
2020-10-31 18:28:43 -0400

    revised function

--- array_practice.js
+++ array_practice.js
@@ -1,36 +1,35 @@
 //arr = [1,0,2,3,0,4,5,0]
 arr = [8,4,5,0,0,0,0,7]
 
+// Count number of zeroes
 let zeroCount = 0 
 arr.forEach(num => {
-  if(num === 0){
-    zeroCount += 1
-  }
+    if(num === 0){
+        zeroCount += 1
+    }
 })
 
-
-console.log(zeroCount)
-
+// Subtract from zero count if zeroes are in index spaces to be overwritten
 for(let j = arr.length - 1; j > arr.length - zeroCount - 1; j --){
-  if(arr[j] === 0){
-    zeroCount -=1
-  }
+    if(arr[j] === 0){
+        zeroCount -=1
+    }
 }
 
-console.log(zeroCount)
-console.log(arr)
-
-// start while loop at original length - 1
-let i = arr.length - zeroCount -1 //index 7 
-console.log(i)
+//Shift indices to the right, with duplicate zeroes
+let i = arr.length - zeroCount -1 
 while (i >= 0){
-  arr[i + zeroCount] = arr[i] 
+     arr[i + zeroCount] = arr[i] 
 
-  if (arr[i] === 0){
-    zeroCount --
-    arr[i + zeroCount] = arr[i] 
-  }
+      if (arr[i] === 0 && zeroCount > 0){
+        zeroCount -- 
+        arr[i + zeroCount] = arr[i] 
+        }
+     else if(arr[i]===0){
+        arr[i + zeroCount] = arr[i] 
+    }
 
-  i--
+    i--
 } 
+
 console.log(arr)
