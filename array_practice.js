aa6e196162807c4df24d2f11779f97f7e9ff38d9
chsohn15 <chsohn15@gmail.com>
2020-10-31 18:14:36 -0400

    duplicate zeroes problem

--- array_practice.js
+++ array_practice.js
@@ -1,1 +1,36 @@
-console.log('hello world')
+//arr = [1,0,2,3,0,4,5,0]
+arr = [8,4,5,0,0,0,0,7]
+
+let zeroCount = 0 
+arr.forEach(num => {
+  if(num === 0){
+    zeroCount += 1
+  }
+})
+
+
+console.log(zeroCount)
+
+for(let j = arr.length - 1; j > arr.length - zeroCount - 1; j --){
+  if(arr[j] === 0){
+    zeroCount -=1
+  }
+}
+
+console.log(zeroCount)
+console.log(arr)
+
+// start while loop at original length - 1
+let i = arr.length - zeroCount -1 //index 7 
+console.log(i)
+while (i >= 0){
+  arr[i + zeroCount] = arr[i] 
+
+  if (arr[i] === 0){
+    zeroCount --
+    arr[i + zeroCount] = arr[i] 
+  }
+
+  i--
+} 
+console.log(arr)
