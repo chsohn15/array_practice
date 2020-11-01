8bf8385efad5011639d36a56829f80bab9b455ee
chsohn15 <chsohn15@gmail.com>
2020-10-31 22:17:38 -0400

    solved duplicate zeros

--- array_practice.js
+++ array_practice.js
@@ -1,35 +1,104 @@
 //arr = [1,0,2,3,0,4,5,0]
-arr = [8,4,5,0,0,0,0,7]
+//arr = [8,4,5,0,0,0,0,7]
+arr= [9,9,9,4,8,0,0,3,7,2,0,0,0,0,9,1,0,0,1,1,0,5,6,3,1,6,0,0,2,3,4,7,0,3,9,3,6,5,8,9,1,1,3,2,0,0,7,3,3,0,5,7,0,8,1,9,6,3,0,8,8,8,8,0,0,5,0,0,0,3,7,7,7,7,5,1,0,0,8,0,0]
+arr2 = [1,5,2,0,6,8,0,6,0]
 
-// Count number of zeroes
-let zeroCount = 0 
-arr.forEach(num => {
-    if(num === 0){
-        zeroCount += 1
+//Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+//Note that elements beyond the length of the original array are not written.
+
+//Do the above modifications to the input array in place, do not return anything from your function.
+
+var duplicateZeros = function(arr) {
+    let zeroCount = 0 
+  
+//Count number of zeros, decrease limit with each zero that needs to be duplicated
+  limit= arr.length -1
+  for(let i = 0; i <= limit; i++){
+    
+    if (arr[i] === 0 && i === limit){
+      zeroCount += 1
     }
-})
-
-// Subtract from zero count if zeroes are in index spaces to be overwritten
-for(let j = arr.length - 1; j > arr.length - zeroCount - 1; j --){
-    if(arr[j] === 0){
-        zeroCount -=1
+    else if (arr[i] === 0){
+      zeroCount += 1
+      limit --
     }
-}
+  }
+ 
+  console.log("Zero count is " + zeroCount)
+  console.log("Limit is " + limit)
+     
+     
+     //Shift indices to the right, with duplicate zeroes
+     let i = limit
+     let a = zeroCount
+ 
+     while (i >= 0){
 
-//Shift indices to the right, with duplicate zeroes
-let i = arr.length - zeroCount -1 
-while (i >= 0){
-     arr[i + zeroCount] = arr[i] 
+        //Have condition to make sure limit plus zeroCount is less than arr.length
+         //Shift indicies to the right
+         if (i + a < arr.length){
+          arr[i + zeroCount] = arr[i] 
+         }
+ 
+         // If the number equals 0, zeroCount is greater than 0 and limit plus zero count is less than or equal to array.length
+        //decrement zeroCount by 1
+         //Add another zero one index left of the previously placed one
+           if (arr[i] === 0 && zeroCount > 0 && (i + a) <= arr.length ){
+             console.log("i:"+ i)
+             console.log("a:" + a)
+             zeroCount -- 
+             arr[i + zeroCount] = arr[i] 
+             console.log(arr)
+             }
+ 
+         i--
+     } 
+ 
+     return arr
+ };
 
-      if (arr[i] === 0 && zeroCount > 0){
-        zeroCount -- 
-        arr[i + zeroCount] = arr[i] 
-        }
-     else if(arr[i]===0){
-        arr[i + zeroCount] = arr[i] 
-    }
+ console.log(duplicateZeros(arr))
+ console.log(duplicateZeros(arr2))
 
-    i--
-} 
 
-console.log(arr)
+//  let zeroCount = 0 
+  
+//   //Count number of zeros, decrease limit with each zero that needs to be duplicated
+//   limit= arr.length -1
+//   for(let i = 0; i <= limit; i++){
+    
+//     if (arr[i] === 0 && i === limit){
+//       zeroCount += 1
+//     }
+//     else if (arr[i] === 0){
+//       zeroCount += 1
+//       limit --
+//     }
+//   }
+ 
+     
+//   let i = limit
+//   let a = zeroCount
+  
+//     //Shift indices to the right, with duplicate zeroes
+//      while (i >= 0){
+
+//         //Have condition to make sure limit plus zeroCount is less than arr.length
+//         //Shift indicies to the right
+//          if (i + a < arr.length){
+//           arr[i + zeroCount] = arr[i] 
+//          }
+         
+//          // If the number equals 0, zeroCount is greater than 0 and limit plus zero count is less than or equal to array.length
+//          //decrement zeroCount by 1
+//          //Add another zero one index left of the previously placed one
+//            if (arr[i] === 0 && zeroCount > 0 && (i + a) <= arr.length ){
+//              zeroCount -- 
+//              arr[i + zeroCount] = arr[i] 
+ 
+//              }
+ 
+//          i--
+//      } 
+//      console.log(arr) 
