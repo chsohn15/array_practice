c7736461c79f7ce4ba88b9db13b29ba64137b75c
chsohn15 <chsohn15@gmail.com>
2020-11-13 20:39:53 -0500

    added array practice

---
+++ hackerrank/electronics_shop.js
@@ -0,0 +1,31 @@
+function getMoneySpent(keyboards, drives, b) {
+    //find difference between budget and keyboard price
+    let totalPriceArray = []
+    keyboards.forEach(boardPrice => {
+      let balance = b - boardPrice 
+  
+      let driveMaxPrice = 0 
+  
+      drives.forEach(drive => {
+        if(drive <= balance && drive > driveMaxPrice){
+          driveMaxPrice = drive
+        }
+      })
+  
+      if(driveMaxPrice === 0){
+        totalPriceArray.push(-1)
+      }
+      else{
+      let mostSpent = boardPrice + driveMaxPrice
+  
+      totalPriceArray.push(mostSpent)
+      }
+    })
+     return(Math.max(...totalPriceArray))
+  
+  }
+
+
+  let keyboards = [3, 1]
+let drives = [3, 7, 8]
+b = 10
