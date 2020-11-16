5b861ffe983f1ef519947e1bcf9beb00d0669946
chsohn15 <chsohn15@gmail.com>
2020-11-16 13:08:36 -0500

    built in sort methods

--- hackerrank/strings_making_anagrams.js
+++ hackerrank/strings_making_anagrams.js
@@ -1,5 +1,11 @@
 function makeAnagram(a, b) {
 
+    //make frequency hash for both strings
+    //iterate through first string
+    //check both objects to see if letter is found
+    //if yes, subtract 1 from both frequency counts
+    //else if letter is found but === 0, then add one to the freq count
+    //add up rest of frequency counts
 
 }
 
