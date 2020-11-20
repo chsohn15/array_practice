0b09f87c2af0504b189bb863ae3c4db96ceac9e5
chsohn15 <chsohn15@gmail.com>
2020-11-20 13:26:50 -0500

    sorting numbers

--- sort/built_in_sorts.js
+++ sort/built_in_sorts.js
@@ -3,3 +3,15 @@
 let stringArray = ["Christina", "Sohn","Koala", "Flatiron"]
 console.log(stringArray.sort())
 
+// Built in sort method accepts optional comparator function  
+// Use comparator function to tell Javascript how to sort
+// Comparator looks at pairs of elements ( a and b ) and determines 
+// sort order based on return value
+// If it returns a negative number, a should come before b
+
+function numberCompare(num1, num2){
+    return num1 - num2
+}
+
+console.log([6, 4, 15, 10].sort(numberCompare))
+
