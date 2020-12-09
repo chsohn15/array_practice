449e325ecaefaf1fd706a4cfc8b343a1b6abb063
chsohn15 <chsohn15@gmail.com>
2020-12-08 20:31:59 -0500

    camel case

---
+++ hackerrank/camelCase.js
@@ -0,0 +1,11 @@
+let s = 'saveChangesInTheEditor'
+function camelcase(s) {
+let counter = 1
+for (let i = 0; i < s.length; i++){
+  if (s[i] === s[i].toUpperCase()){
+    counter ++
+   }
+  }
+return counter
+}
+camelcase(s)
